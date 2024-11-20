008e72061fd5fe6e5f759b5946a6ef1a1c33b0fe
EthanForrestCarr <ethan@ethancarr.com>
2024-11-20 15:20:28 -0600

    refactored auth.ts

--- server/src/schemas/resolvers.ts
+++ server/src/schemas/resolvers.ts
@@ -1,4 +1,4 @@
-import { User } from '../models/User.js';
+import User from '../models/User.js';
 import { AuthenticationError } from 'apollo-server';
 
 const resolvers = {
--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,39 +1,28 @@
-import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-import dotenv from 'dotenv';
-dotenv.config();
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
 
-interface JwtPayload {
-  _id: unknown;
-  username: string;
-  email: string,
-}
+// Function to extract and verify the token for GraphQL
+const authMiddleware = (req: any) => {
+  let token = req.headers.authorization || '';
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
+  // Check for Bearer token and extract it
+  if (token.startsWith('Bearer ')) {
+    token = token.substring(7, token.length).trim();
+  }
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
+  if (!token) {
+    return { user: null };
+  }
 
-    const secretKey = process.env.JWT_SECRET_KEY || '';
-
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); // Forbidden
-      }
-
-      req.user = user as JwtPayload;
-      return next();
-    });
-  } else {
-    res.sendStatus(401); // Unauthorized
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    return { user: data };
+  } catch {
+    console.log('Invalid token');
+    return { user: null };
   }
 };
 
-export const signToken = (username: string, email: string, _id: unknown) => {
-  const payload = { username, email, _id };
-  const secretKey = process.env.JWT_SECRET_KEY || '';
-
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
-};
+export default authMiddleware;

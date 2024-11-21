0bfef3cc3aa997ac7df6f5135130adc8d8e7a7b2
EthanForrestCarr <ethan@ethancarr.com>
2024-11-20 22:39:45 -0600

    refactored App.tsx again.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,34 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
 import Navbar from './components/Navbar';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
   cache: new InMemoryCache(),
 });
 
-function App() {
+const App = () => {
   return (
-    <>
-      <ApolloProvider client={client}>
-        <Navbar />
-        <Outlet />
-      </ApolloProvider>
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<SearchBooks />} />
+            <Route path="/saved" element={<SavedBooks />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/signup" element={<SignupForm />} />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
